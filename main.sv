b6ff5ed90ee7f54f3538894177f487d7892be4f5
Sawyer <sawyer.c.theis@gmail.com>
2024-10-02 17:36:46 -0500

    we are cooking

--- main.sv
+++ main.sv
@@ -1,4 +1,11 @@
-module main(input logic switches[2:0], buttons[1:0], realClk);
+module main(
+	input logic [2:0] switches, 
+	input logic [1:0] buttons, 
+	input logic realClk, 
+	output logic [4:0] outputLED, 
+	output logic [3:0] stateLED, 
+	output logic [6:0] stateDisplay, changeDisplay
+);
 
-and(switches[0], realClk, buttons[1]);
+
 endmodule
